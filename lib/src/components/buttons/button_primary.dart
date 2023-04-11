a812961b14e03e5117bd8a1ef4033e562d3e57b7
Lílian Carneiro Ramalho <60183687+lilianramalho@users.noreply.github.com>
2023-04-10 23:12:26 -0300

    fix button style

--- lib/src/components/buttons/button_primary.dart
+++ lib/src/components/buttons/button_primary.dart
@@ -3,41 +3,42 @@ import 'package:meiuca_challenge/src/tokens/borders/meiuca_border_radius.dart';
 import 'package:meiuca_challenge/src/tokens/colors/band_colors.dart';
 import 'package:meiuca_challenge/src/tokens/colors/neutral_colors.dart';
 import 'package:meiuca_challenge/src/tokens/fonts/font_size.dart';
-import 'package:meiuca_challenge/src/tokens/fonts/line_height.dart';
 import 'package:meiuca_challenge/src/tokens/fonts/meiuca_font_weight.dart';
 import 'package:meiuca_challenge/src/tokens/spacing/spacing_squish.dart';
 
 class ButtonPrimary extends StatelessWidget {
-  const ButtonPrimary(
-      {super.key,
-      required this.width,
-      required this.height,
-      required this.text});
-  final double width;
-  final double height;
+  const ButtonPrimary({super.key, required this.text});
   final String text;
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      child: Container(
-        padding: SpacingSquish.xs,
-        decoration:const BoxDecoration(
-          color: BrandColors.colorBrandPrimary03,
-          borderRadius: MeiucaBorderRadius.none,
+    return ElevatedButton(
+        onPressed: () {},
+        style: ButtonStyle(
+          padding: MaterialStateProperty.all<EdgeInsets>(
+            SpacingSquish.xs,
+          ),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            const RoundedRectangleBorder(
+              borderRadius: MeiucaBorderRadius.none,
+            ),
+          ),
+          backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+            (Set<MaterialState> states) {
+              if (states.contains(MaterialState.pressed)) {
+                return BrandColors.colorBrandPrimary02;
+              }
+              return BrandColors
+                  .colorBrandPrimary03; // Defer to the widget's default.
+            },
+          ),
         ),
-        width: width,
-        height: height,
         child: Text(
           text,
           style: const TextStyle(
-            fontSize: FontSize.fontSizeSM,
-            height: LineHeight.tight,
-            color: NeutralColors.colorNeutral02,
-            fontWeight: MeiucaFontWeight.fontWeightMedium,
-          ),
-        ),
-      ),
-    );
+              color: NeutralColors.colorNeutral05,
+              fontWeight: MeiucaFontWeight.fontWeightMedium,
+              fontSize: FontSize.fontSizeSM),
+        ));
   }
 }
