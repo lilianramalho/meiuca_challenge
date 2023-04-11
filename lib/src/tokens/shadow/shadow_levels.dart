4ca61a9899bbfb7798184eb7ccbe4c48e280e388
Lílian Carneiro Ramalho <60183687+lilianramalho@users.noreply.github.com>
2023-04-11 19:46:35 -0300

    fix shadow

--- lib/src/tokens/shadow/shadow_levels.dart
+++ lib/src/tokens/shadow/shadow_levels.dart
@@ -4,5 +4,6 @@ class ShadowLevels {
 
 static const Offset level1 = Offset(8, 4);
 static const Offset level2 = Offset(24, 8);
-static const Offset level3 = Offset(48, 16);
+static const Offset level3 = Offset(32, 16);
+static const Offset level4 = Offset(48, 16);
 }
